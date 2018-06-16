827601d4865b4b4863b13bdeebfe835ae99cef55
Pat Graham <pdgra1@student.monash.edu>
2018-06-16 11:21:45 +1000

    init

---
+++ log.py
@@ -0,0 +1,12 @@
+# This file appends the bl_log.txt file with the current date and time each
+# time the script is called.
+
+import datetime
+
+now = datetime.datetime.now()
+
+f = open('/home/pi/Documents/BL_Testing/bl_log.txt', 'a')
+
+f.write(str(now)+"\n")
+
+f.close()
---
+++ start.py
@@ -0,0 +1,8 @@
+# This file prints a "testing start" statement into the log file so you can 
+# work out where the pi was turned on
+
+f = open('/home/pi/Documents/BL_Testing/bl_log.txt', 'a')
+
+f.write("\n\nPi Turned on, Restarted Testing here!\n")
+
+f.close()
